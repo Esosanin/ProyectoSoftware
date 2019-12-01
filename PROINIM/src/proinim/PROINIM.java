b80bbba563b0d930fe04432dbd253d76e55e43c9
JoPulido123 <56531161+JoPulido123@users.noreply.github.com>
2019-12-01 13:30:10 -0700

    Add files via upload
    Pruebas respecto interfaz en swing,

---
+++ PROINIM/src/proinim/PROINIM.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proinim;
+
+/**
+ *
+ * @author usuariolocal
+ */
+public class PROINIM {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String user = "postgres";
+        String password = "Universo7788";
+        String driver = "org.postgresql.Driver";
+        String host = "localhost";
+        String nameDB = "PROINIM";
+        String portDB = "5432";
+        String url = "jdbc:postgresql://localhost:"+portDB+"/"+nameDB;
+        Base0 enter = new Base0();
+        Base0.create(url, user, password, driver);
+        
+        enter.setVisible(true);
+        enter.setLocationRelativeTo(null);
+        enter.setTitle("DataBase DVDRental");
+        
+    }
+    
+}
